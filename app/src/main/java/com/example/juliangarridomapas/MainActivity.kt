bf9a9ad47824d3bb5a95a0f6e6efc1d571c52ed4
Juliang325 <133649655+Juliang325@users.noreply.github.com>
2024-02-15 14:39:16 +0100

    No tengo ubicacion pintada ns pq

--- app/src/main/java/com/example/juliangarridomapas/MainActivity.kt
+++ app/src/main/java/com/example/juliangarridomapas/MainActivity.kt
@@ -1,38 +1,93 @@
 package com.example.juliangarridomapas
 
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.fragment.app.FragmentManager
+import android.util.Log
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 
-class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+class MainActivity : AppCompatActivity(), OnMapReadyCallback, LocationListener {
 
     private lateinit var map:GoogleMap
+    private lateinit var locationManager: LocationManager
+    private val LOCATION_PERMISSION_REQUEST_CODE = 1001
+    private  var latitude : Double =0.0
+    private  var longitude : Double =0.0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         createFragment()
-    }
+        locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), LOCATION_PERMISSION_REQUEST_CODE)
+        } else {
+            // Permiso de ubicación otorgado, obtener la ubicación actual
 
+        }
+    }
     private fun createFragment() {
         val mapFragment = supportFragmentManager.findFragmentById(R.id.maps) as SupportMapFragment
         mapFragment.getMapAsync(this)
     }
-
     override fun onMapReady(googleMap: GoogleMap) {
         map =googleMap
         createMarker()
     }
-
     private fun createMarker() {
-        val coordinates= LatLng(1.2, 1.2)
+        val coordinates= LatLng(latitude, longitude)
         val marker = MarkerOptions().position(coordinates).title("Mi lugar especial")
         map.addMarker(marker)
+        map.animateCamera(
+            CameraUpdateFactory.newLatLngZoom(coordinates, 18f),
+            4000,
+            null
+        )
+    }
+    private fun obtainLocation() {
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            return
+        }
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0f, this)
+    }
+    override fun onLocationChanged(location: Location) {
+        // La ubicación actual se encuentra en el objeto 'location'
+        latitude = location.latitude
+        longitude = location.longitude
+        // Utiliza la ubicación como sea necesario
+        Log.d("Ubicacion", "Latitud: $latitude, Longitud: $longitude")
+    }
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            LOCATION_PERMISSION_REQUEST_CODE -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // Permiso de ubicación otorgado, obtener la ubicación actual
+                    obtainLocation()
+                } else {
+                    // Permiso de ubicación denegado, informar al usuario o tomar medidas alternativas
+                    // ...
+                }
+                return
+            }
+        }
     }
 }
